abfc6480aa2e926bac2049dc1e3174c295c98da6
jeslaa <jesperlaaksonen@hotmail.com>
2023-09-27 13:27:49 +0200

    Navbar completed started on home section

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Navbar from "../src/components/Navbar"
+import { SelectedPage } from "./shared/types"
+import Home from "../src/components/Home"
 
-enum SelectedPage {
-  Home = "home",
-  Benfits = "benefits",
-  OurClasses = "ourclasses",
-  ContactUs = "contactus"
-}
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>("home")
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0){
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home)
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
   return (
     <div className='app bg-gray-20'>
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Home setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
